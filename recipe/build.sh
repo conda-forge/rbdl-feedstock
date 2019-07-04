6d7f3cc152ce077479392c09ccf40180cf3e2995
Pariterre <pariterre@hotmail.com>
2019-07-04 14:03:21 +0200

    force the lib dir to be "lib"

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,8 @@ cd build
 
 cmake ../ \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DCMAKE_BUILD_TYPE=Release
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_LIBDIR=lib
 
 make -j $CPU_COUNT
 make install
