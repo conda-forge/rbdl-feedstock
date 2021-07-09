1c14d36a678d3d9b008878b9b903fb3141984469
Pariterre <pariterre@hotmail.com>
2021-07-09 09:44:11 +0200

    removed "

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,7 +6,7 @@ cmake ../ ^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
     -DRBDL_BUILD_STATIC=ON ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DRBDL_USE_CASADI_MATH="%rbdl_use_casadi_math%"
+    -DRBDL_USE_CASADI_MATH=%rbdl_use_casadi_math%
 
 cmake --build ./ ^
     --config Release ^
