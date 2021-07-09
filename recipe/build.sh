58a04391ed22d130ca08172173c26829dc3ae9d7
Pariterre <pariterre@hotmail.com>
2021-07-09 09:21:51 +0200

    Idem

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,6 @@ cmake ../ \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_LIBDIR=lib \
-      -DMATH_LIBRARY_BACKEND=$backend_name_in_build
+      -DRBDL_USE_CASADI_MATH=$rbdl_use_casadi_math
 
 ninja install
