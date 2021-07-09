4bab7b2e1ddf8255aab3043e2c21e9744a71e760
Pariterre <pariterre@hotmail.com>
2021-07-09 10:49:05 +0200

    Cleaned a bit

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mkdir build
 cd build
-echo coucou
+
 echo $backend
-echo $backend_name_in_build
+echo $rbdl_use_casadi_math
 cmake ../ \
       -GNinja \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
