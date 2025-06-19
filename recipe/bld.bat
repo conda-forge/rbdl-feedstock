9d1f29bae73ddbe83cf3db3ddc3a553740c8533b
Pariterre <benjamin.michaud.phq@gmail.com>
2025-06-19 10:22:14 -0400

    Trying to fix windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,6 +3,8 @@ cd build
 
 cmake ../ ^
     -GNinja ^
+    -DCMAKE_C_COMPILER=%CC% ^
+    -DCMAKE_CXX_COMPILER=%CXX% ^
     -DCMAKE_INSTALL_PREFIX="%PREFIX%" ^
     -DRBDL_BUILD_STATIC=OFF ^
     -DCMAKE_BUILD_TYPE=Release ^
